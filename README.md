f1ff13c9d665ae09a169620b08dafbfd59098c06
jigkoh3 <jigkoh3@hotmail.com>
2015-09-30 09:03:01 +0700

    add test module

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.12.1.
+## insatll 
+
+Run 'npm install'
+Run 'bower install'
 
 ## Build & development
 
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,7 @@
  */
 angular
   .module('fec3App', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+    'ngRoute'
   ])
   .config(function ($routeProvider) {
     $routeProvider
